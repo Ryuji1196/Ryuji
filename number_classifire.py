1a3890db72f81ecf945ab0bb3c1dd5f61752ca4f
Your Name <you@example.com>
2021-06-05 22:23:20 +0900

    (R)

--- number_classifire.py
+++ number_classifire.py
@@ -6,29 +6,32 @@ from keras.utils.np_utils import to_categorical
 from keras.utils.vis_utils import plot_model
 import numpy as np
 import matplotlib.pyplot as plt
-
-#モデルの保存
-
-#データのロード
+# データのロード
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-#データの整形
-X_train = X_train.reshape([-1,28,28,1])
-X_test = X_test.reshape([-1,28,28,1])
+# 今回は全データのうち、学習には300、テストには100個のデータを使用します。
+# Convレイヤーは4次元配列を受け取ります。（バッチサイズx縦x横xチャンネル数）
+# MNISTのデータはRGB画像ではなくもともと3次元のデータとなっているので予め4次元に変換します。
+X_train = X_train.reshape(-1, 28, 28, 1)
+X_test = X_test.reshape(-1, 28, 28, 1)
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
-#モデルの定義
+# モデルの定義
 model = Sequential()
-model.add(Conv2D(filters = 32, kernel_size = (3,3), input_shape = (28,28,1)))
-model.add(Activation("relu"))
+model.add(Conv2D(filters=32, kernel_size=(3, 3),input_shape=(28,28,1)))
+model.add(Activation('relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
 model.add(Dense(128))
-model.add(Activation("relu"))
+model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(64))
-model.add(Activation("relu"))
-model.add(Dense(1))
-model.add(Activation("sigmoid"))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
 
 model.compile(loss='categorical_crossentropy',
               optimizer='adadelta',
@@ -40,10 +43,10 @@ model.fit(X_train, y_train,
           verbose=1,
           validation_data=(X_test, y_test))
 
-#精度の評価
-scores = model.evaluate(X_test,y_test, verbose=1)
-print("test loss:", scores[0])
-print("test accuracy:", scores[1])
+# 精度の評価
+scores = model.evaluate(X_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
 
 # データの可視化（テストデータの先頭の10枚）
 for i in range(10):
@@ -65,4 +68,5 @@ if not os.path.exists(result_dir):
 # 重みを保存
 model.save(os.path.join(result_dir, 'model.h5'))
 
-files.download( '/content/results/model.h5' ) 
+
+files.download( 'For_Aidemy/model.h5' ) 
